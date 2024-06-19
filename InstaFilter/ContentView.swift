c98bfcfb365b46879446b5294f6c57944aeba80a
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-06-19 23:13:21 +0100

    confirmationDiaolog

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var blurAmount = 0.0
+    @State private var showingConfirmation = false
+    @State private var backgroundColour = Color.white
     
     
     var body: some View {
@@ -23,6 +25,22 @@ struct ContentView: View {
             
             Button("Random Blur") {
                 blurAmount = Double.random(in: 0...20)
+            }
+            .padding(.bottom, 50)
+            
+            Button("Tap Me") {
+                showingConfirmation.toggle()
+            }
+            .frame(width: 300, height: 300)
+            .foregroundStyle(.purple)
+            .background(backgroundColour)
+            .confirmationDialog("Change Background", isPresented: $showingConfirmation) {
+                Button("Red") { backgroundColour = .red}
+                Button("Green") { backgroundColour = .green}
+                Button("Blue") { backgroundColour = .blue}
+                Button("Cancel", role: .cancel) {}
+            } message: {
+                Text("Select a new colour")
             }
         }
 
