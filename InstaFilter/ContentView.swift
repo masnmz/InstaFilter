62a71019d97fe8061586203d0d345dc17cf9122d
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-06-20 14:45:37 +0100

    PhotoPicker, ShareLink

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -5,49 +5,49 @@
 //  Created by Mehmet Alp Sönmez on 19/06/2024.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    
-    
+    @State private var pickerItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
+
     var body: some View {
         VStack {
-            image?
-                .resizable()
-                .scaledToFit()
+            PhotosPicker( selection: $pickerItems, maxSelectionCount: 3, matching: .any(of: [
+                .images, .not(.screenshots)])) {
+                Label("Select a picture", systemImage: "photo")
+            }
+            
+            ScrollView(.horizontal) {
+                ForEach(0..<selectedImages.count, id:\.self) { i in
+                    selectedImages[i]
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            
+            ShareLink(item: URL(string: "https://www.hackingwithswift.com")!) {
+                Label("Spread the word about Swift", systemImage: "swift")
+            }
+            
+            let example = Image(.example)
+            
+            ShareLink(item: example, preview: SharePreview("House", image: example)) {
+                Label("Click to share", systemImage: "airplane")
+            }
         }
-        .onAppear(perform: loadImage)
-
-    }
-    
-    func loadImage() {
-        let inputImage = UIImage(resource: .example)
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.twirlDistortion()
-        
-        currentFilter.inputImage = beginImage
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(amount, forKey: kCIInputIntensityKey)}
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)}
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)}
-        
-        
-        currentFilter.radius = 2000
-        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
-        
-        guard let outputImage = currentFilter.outputImage else { return }
-        guard let cgImg = context.createCGImage(outputImage, from: outputImage.extent)
-        else { return }
-        
-        let uiImage = UIImage(cgImage: cgImg)
-        image = Image(uiImage: uiImage)
+        .onChange(of: pickerItems) {
+            Task {
+                selectedImages.removeAll()
+                
+                for item in pickerItems {
+                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
+                        selectedImages.append(loadedImage)
+                    }
+                }
+            }
+        }
     }
 }
 
